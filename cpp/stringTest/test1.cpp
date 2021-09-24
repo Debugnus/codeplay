aee80ed48ee50296944cec317bab7fa7e5a23737
Debugnus <debugnus@gmail.com>
2021-09-24 20:15:20 +0930

    fix correct code for comparing and using strings

--- cpp/stringTest/test1.cpp
+++ cpp/stringTest/test1.cpp
@@ -1,35 +1,42 @@
 #include <iostream>
 #include <string>
+#include <thread>
+#include <chrono>
+
+using namespace std::chrono_literals;
 
 int main() {
-	std::string testText = ".....................";
+	std::string testText(".....................");
 
 	int Testo = testText.length();
 
 	std::cout << Testo << std::endl;
-	
+
+	int HashCount = 0; 
 // check if # x 4
 // 
 	
 	for (int i = 0; i < Testo; i++){
-		int HashCount = 0;
-		if (Testo[i] == "."){
-			testText.replace(Testo[i], 1, "#");
+
+		// error: subscripted value is not an array, pointer, or vector
+		if (testText.compare(".") != 0){
+			testText.replace(i, 1, "#");
 			HashCount++;
 			if (HashCount == 4){
 				std::cout << "[" << testText << "]" << std::endl;
 				
-				// timer
+				std::this_thread::sleep_for(2000ms);
 
-				// then reset HashCount
+				HashCount = 0;
+
 			}
 		} else {
 
 		}
 	}
 
-	testText.replace(1, 1, "5");
-	std::cout << testText[1];
+	//testText.replace(1, 1, "5");
+	//std::cout << testText[1];
 
 	return 0;
 }
