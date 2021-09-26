a38e8c209b832015bb76106e85498fb73255e265
debugnus <debugnus@gmail.com>
2021-09-26 21:39:58 +0930

    Debugged and corrected code (needs cleanup)

--- cpp/stringTest/test1.cpp
+++ cpp/stringTest/test1.cpp
@@ -9,22 +9,27 @@ int main() {
 	std::string testText("........................................");
 
 	int Testo = testText.length();
+	double dots = Testo;
 
 	int HashCount = 0; // used to count and limit to 4 hashes each time
-	int HashTotal = 0; // used to calculater percentage
-	int CharCount = 0; // is this even required for counting instead of using i ? 
-	for (int i = 0; i < Testo; i++){
-		CharCount = i;
+	int iHash = 0;
+
+	int HashTotal = 0;		// ---- Change to floating point number 0.0
+
+
+	for (int i = 0.0; i < Testo; i++){
 		if (testText.compare(".") != 0){	// test if Period character found
 			testText.replace(i, 1, "#");	// replace Period with Hash
 			HashCount++;	// increment HashCount by 1
+//			std::cout << "\n" << i << "\n";
 			if (HashCount == 4){	// is HashCount 4?
-				HashTotal = (CharCount/Testo)*100;	// why calculating to 0 ?
+				iHash += HashCount; 
+				HashTotal = (iHash/dots)*100;	// why calculating to 0 ?
 				std::cout << "[" << testText << "]" << HashTotal << std::endl;
 				
 				std::this_thread::sleep_for(2000ms);
 
-				HashCount = 0;
+				HashCount = 0.0;
 
 			}
 		} else {
