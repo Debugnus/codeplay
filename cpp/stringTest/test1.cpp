19e5443a290bf0e8a94f20a1a9d11c90ad333c27
debugnus <debugnus@gmail.com>
2021-09-26 14:11:58 +0930

    add percentage calculation, however, this is not working. Also add comments.

--- cpp/stringTest/test1.cpp
+++ cpp/stringTest/test1.cpp
@@ -6,21 +6,21 @@
 using namespace std::chrono_literals;
 
 int main() {
-	std::string testText(".....................");
+	std::string testText("........................................");
 
 	int Testo = testText.length();
 
-	std::cout << Testo << std::endl;
-
-	int HashCount = 0; 
-	
+	int HashCount = 0; // used to count and limit to 4 hashes each time
+	int HashTotal = 0; // used to calculater percentage
+	int CharCount = 0; // is this even required for counting instead of using i ? 
 	for (int i = 0; i < Testo; i++){
-
-		if (testText.compare(".") != 0){
-			testText.replace(i, 1, "#");
-			HashCount++;
-			if (HashCount == 4){
-				std::cout << "[" << testText << "]" << std::endl;
+		CharCount = i;
+		if (testText.compare(".") != 0){	// test if Period character found
+			testText.replace(i, 1, "#");	// replace Period with Hash
+			HashCount++;	// increment HashCount by 1
+			if (HashCount == 4){	// is HashCount 4?
+				HashTotal = (CharCount/Testo)*100;	// why calculating to 0 ?
+				std::cout << "[" << testText << "]" << HashTotal << std::endl;
 				
 				std::this_thread::sleep_for(2000ms);
 
