039f9f212be1942a5a5345240c54a18887dd56ac
Debugnus <debugnus@gmail.com>
2021-09-28 12:12:22 +0930

    code clean up and add more comments

--- cpp/stringTest/test1.cpp
+++ cpp/stringTest/test1.cpp
@@ -9,27 +9,25 @@ int main() {
 	std::string testText("........................................");
 
 	int Testo = testText.length();
-	double dots = Testo;
+	double dots = Testo; // convert Testo to double for calculating percentages
 
 	int HashCount = 0; // used to count and limit to 4 hashes each time
-	int iHash = 0;
+	int iHash = 0; // for saving HashCounts into
 
-	int HashTotal = 0;		// ---- Change to floating point number 0.0
+	int HashTotal = 0;		
 
-
-	for (int i = 0.0; i < Testo; i++){
+	for (int i = 0; i < Testo; i++){
 		if (testText.compare(".") != 0){	// test if Period character found
 			testText.replace(i, 1, "#");	// replace Period with Hash
 			HashCount++;	// increment HashCount by 1
-//			std::cout << "\n" << i << "\n";
 			if (HashCount == 4){	// is HashCount 4?
 				iHash += HashCount; 
-				HashTotal = (iHash/dots)*100;	// why calculating to 0 ?
+				HashTotal = (iHash/dots)*100;
 				std::cout << "[" << testText << "]" << HashTotal << std::endl;
 				
 				std::this_thread::sleep_for(2000ms);
 
-				HashCount = 0.0;
+				HashCount = 0;
 
 			}
 		} else {
